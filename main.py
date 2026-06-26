2832621ea686ba338f194c253a182e145813edda
Akhil <akhil@email.com>
2026-06-26 15:39:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,103 +1,102 @@
-import json
-import unittest
-from datetime import datetime, timezone
+import json, unittest, datetime
+
+with open("./data-1.json","r") as f:
+    jsonData1 = json.load(f)
+with open("./data-2.json","r") as f:
+    jsonData2 = json.load(f)
+with open("./data-result.json","r") as f:
+    jsonExpectedResult = json.load(f)
 
 
-def convertFromFormat1(jsonObject):
-    location_parts = jsonObject["location"].split("/")
-    location_keys = ["country", "city", "area", "factory", "section"]
+def convertFromFormat1 (jsonObject):
 
-    return {
-        "deviceID": jsonObject["deviceID"],
-        "deviceType": jsonObject["deviceType"],
-        "timestamp": jsonObject["timestamp"],
-        "location": dict(zip(location_keys, location_parts)),
-        "data": {
-            "status": jsonObject["operationStatus"],
-            "temperature": jsonObject["temp"]
+    locationParts = jsonObject['location'].split('/')
+
+    result = {
+        'deviceID': jsonObject['deviceID'],
+        'deviceType': jsonObject['deviceType'],
+        'timestamp': jsonObject['timestamp'],
+        'location': {
+            'country': locationParts[0],
+            'city': locationParts[1],
+            'area': locationParts[2],
+            'factory': locationParts[3],
+            'section': locationParts[4]
+        },
+        'data': {
+            'status': jsonObject['operationStatus'],
+            'temperature': jsonObject['temp']
         }
     }
 
+    return result
 
-def convertFromFormat2(jsonObject):
-    # fromisoformat doesn't handle trailing 'Z' in Python < 3.11
-    iso_str = jsonObject["timestamp"].rstrip("Z")
-    dt = datetime.fromisoformat(iso_str).replace(tzinfo=timezone.utc)
-    timestamp_ms = int(dt.timestamp() * 1000)
+def convertFromFormat2 (jsonObject):
 
-    return {
-        "deviceID": jsonObject["device"]["id"],
-        "deviceType": jsonObject["device"]["type"],
-        "timestamp": timestamp_ms,
-        "location": {
-            "country": jsonObject["country"],
-            "city": jsonObject["city"],
-            "area": jsonObject["area"],
-            "factory": jsonObject["factory"],
-            "section": jsonObject["section"]
+    date = datetime.datetime.strptime(
+        jsonObject['timestamp'],
+        '%Y-%m-%dT%H:%M:%S.%fZ'
+    )
+    timestamp = round(
+        (date - datetime.datetime(1970, 1, 1)).total_seconds() * 1000
+    )
+
+    result = {
+        'deviceID': jsonObject['device']['id'],
+        'deviceType': jsonObject['device']['type'],
+        'timestamp': timestamp,
+        'location': {
+            'country': jsonObject['country'],
+            'city': jsonObject['city'],
+            'area': jsonObject['area'],
+            'factory': jsonObject['factory'],
+            'section': jsonObject['section']
         },
-        "data": jsonObject["data"]
+        'data': jsonObject['data']
     }
 
-
-class TestTelemetryConversion(unittest.TestCase):
-
-    def setUp(self):
-        with open("data-1.json") as f:
-            self.format1 = json.load(f)
-        with open("data-2.json") as f:
-            self.format2 = json.load(f)
-        with open("data-result.json") as f:
-            self.expected = json.load(f)
-
-    def test_format1_deviceID(self):
-        result = convertFromFormat1(self.format1)
-        self.assertEqual(result["deviceID"], self.expected["deviceID"])
-
-    def test_format1_deviceType(self):
-        result = convertFromFormat1(self.format1)
-        self.assertEqual(result["deviceType"], self.expected["deviceType"])
-
-    def test_format1_timestamp(self):
-        result = convertFromFormat1(self.format1)
-        self.assertEqual(result["timestamp"], self.expected["timestamp"])
-
-    def test_format1_location(self):
-        result = convertFromFormat1(self.format1)
-        self.assertEqual(result["location"], self.expected["location"])
-
-    def test_format1_data(self):
-        result = convertFromFormat1(self.format1)
-        self.assertEqual(result["data"], self.expected["data"])
-
-    def test_format1_full(self):
-        result = convertFromFormat1(self.format1)
-        self.assertEqual(result, self.expected)
-
-    def test_format2_deviceID(self):
-        result = convertFromFormat2(self.format2)
-        self.assertEqual(result["deviceID"], self.expected["deviceID"])
-
-    def test_format2_deviceType(self):
-        result = convertFromFormat2(self.format2)
-        self.assertEqual(result["deviceType"], self.expected["deviceType"])
-
-    def test_format2_timestamp(self):
-        result = convertFromFormat2(self.format2)
-        self.assertEqual(result["timestamp"], self.expected["timestamp"])
-
-    def test_format2_location(self):
-        result = convertFromFormat2(self.format2)
-        self.assertEqual(result["location"], self.expected["location"])
-
-    def test_format2_data(self):
-        result = convertFromFormat2(self.format2)
-        self.assertEqual(result["data"], self.expected["data"])
-
-    def test_format2_full(self):
-        result = convertFromFormat2(self.format2)
-        self.assertEqual(result, self.expected)
+    return result
 
 
-if __name__ == "__main__":
-    unittest.main(verbosity=2)
+def main (jsonObject):
+
+    result = {}
+
+    if (jsonObject.get('device') == None):
+        result = convertFromFormat1(jsonObject)
+    else:
+        result = convertFromFormat2(jsonObject)
+
+    return result
+
+
+class TestSolution(unittest.TestCase):
+
+    def test_sanity(self):
+
+        result = json.loads(json.dumps(jsonExpectedResult))
+        self.assertEqual(
+            result,
+            jsonExpectedResult
+        )
+
+    def test_dataType1(self):
+
+        result = main (jsonData1)
+        self.assertEqual(
+            result,
+            jsonExpectedResult,
+            'Converting from Type 1 failed'
+        )
+
+    def test_dataType2(self):
+
+        result = main (jsonData2)
+        self.assertEqual(
+            result,
+            jsonExpectedResult,
+            'Converting from Type 2 failed'
+        )
+
+if __name__ == '__main__':
+    unittest.main()
